889d2b3530285aaf6b081ae25988d325322b3a2d
magnus <magnus.wahlstrand@gmail.com>
2025-01-01 21:12:38 +0100

    move git plugins and add fugitive

--- init.lua
+++ init.lua
@@ -151,18 +151,6 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   { import = 'custom/plugins' },
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
@@ -272,112 +260,6 @@ require('lazy').setup({
       },
     },
   },
-  --
-  --{ -- Fuzzy Finder (files, lsp, etc)
-  --  'nvim-telescope/telescope.nvim',
-  --  event = 'VimEnter',
-  --  branch = '0.1.x',
-  --  dependencies = {
-  --    'nvim-lua/plenary.nvim',
-  --    { -- If encountering errors, see telescope-fzf-native README for installation instructions
-  --      'nvim-telescope/telescope-fzf-native.nvim',
-
-  --      -- `build` is used to run some command when the plugin is installed/updated.
-  --      -- This is only run then, not every time Neovim starts up.
-  --      build = 'make',
-
-  --      -- `cond` is a condition used to determine whether this plugin should be
-  --      -- installed and loaded.
-  --      cond = function()
-  --        return vim.fn.executable 'make' == 1
-  --      end,
-  --    },
-  --    { 'nvim-telescope/telescope-ui-select.nvim' },
-
-  --    -- Useful for getting pretty icons, but requires a Nerd Font.
-  --    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-  --  },
-  --  config = function()
-  --    -- Telescope is a fuzzy finder that comes with a lot of different things that
-  --    -- it can fuzzy find! It's more than just a "file finder", it can search
-  --    -- many different aspects of Neovim, your workspace, LSP, and more!
-  --    --
-  --    -- The easiest way to use Telescope, is to start by doing something like:
-  --    --  :Telescope help_tags
-  --    --
-  --    -- After running this command, a window will open up and you're able to
-  --    -- type in the prompt window. You'll see a list of `help_tags` options and
-  --    -- a corresponding preview of the help.
-  --    --
-  --    -- Two important keymaps to use while in Telescope are:
-  --    --  - Insert mode: <c-/>
-  --    --  - Normal mode: ?
-  --    --
-  --    -- This opens a window that shows you all of the keymaps for the current
-  --    -- Telescope picker. This is really useful to discover what Telescope can
-  --    -- do as well as how to actually do it!
-
-  --    -- [[ Configure Telescope ]]
-  --    -- See `:help telescope` and `:help telescope.setup()`
-  --    require('telescope').setup {
-  --      -- You can put your default mappings / updates / etc. in here
-  --      --  All the info you're looking for is in `:help telescope.setup()`
-  --      --
-  --      -- defaults = {
-  --      --   mappings = {
-  --      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-  --      --   },
-  --      -- },
-  --      -- pickers = {}
-  --      extensions = {
-  --        ['ui-select'] = {
-  --          require('telescope.themes').get_dropdown(),
-  --        },
-  --      },
-  --    }
-
-  --    -- Enable Telescope extensions if they are installed
-  --    pcall(require('telescope').load_extension, 'fzf')
-  --    pcall(require('telescope').load_extension, 'ui-select')
-
-  --    -- See `:help telescope.builtin`
-  --    local builtin = require 'telescope.builtin'
-  --    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-  --    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-  --    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-  --    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-  --    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-  --    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-  --    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-  --    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-  --    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-  --    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-  --    -- Slightly advanced example of overriding default behavior and theme
-  --    vim.keymap.set('n', '<leader>/', function()
-  --      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-  --      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-  --        winblend = 10,
-  --        previewer = false,
-  --      })
-  --    end, { desc = '[/] Fuzzily search in current buffer' })
-
-  --    -- It's also possible to pass additional configuration options.
-  --    --  See `:help telescope.builtin.live_grep()` for information about particular keys
-  --    vim.keymap.set('n', '<leader>s/', function()
-  --      builtin.live_grep {
-  --        grep_open_files = true,
-  --        prompt_title = 'Live Grep in Open Files',
-  --      }
-  --    end, { desc = '[S]earch [/] in Open Files' })
-
-  --    -- Shortcut for searching your Neovim configuration files
-  --    vim.keymap.set('n', '<leader>sn', function()
-  --      builtin.find_files { cwd = vim.fn.stdpath 'config' }
-  --    end, { desc = '[S]earch [N]eovim files' })
-  --  end,
-  --},
-
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -451,31 +333,6 @@ require('lazy').setup({
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
-          -- map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-          -- Find references for the word under your cursor.
-          -- map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
-          --map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
-          --map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
-          --map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
-          -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
-          -- map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
@@ -488,7 +345,14 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-          -- The following two autocommands are used to highlight references of the
+          -- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+          map('<leader>gr', vim.lsp.buf.references, '[G]oto [R]eferences')
+          map('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+          map('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+          map('<leader>ds', vim.lsp.buf.document_symbol, '[D]ocument [S]ymbols')
+          map('<leader>ws', vim.lsp.buf.workspace_symbol, '[W]orkspace [S]ymbols') -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
           --
@@ -859,9 +723,9 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'custom.plugins.git-stuff', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
--- lua/custom/plugins/refactoring.lua
+++ lua/custom/plugins/refactoring.lua
@@ -8,6 +8,23 @@ return {
     lazy = false,
     config = function()
       require('refactoring').setup()
+      -- You can also use below = true here to to change the position of the printf
+      -- statement (or set two remaps for either one). This remap must be made in normal mode.
+      vim.keymap.set('n', '<leader>rp', function()
+        require('refactoring').debug.printf { below = false }
+      end)
+
+      -- Print var
+
+      vim.keymap.set({ 'x', 'n' }, '<leader>rv', function()
+        require('refactoring').debug.print_var()
+      end)
+      -- Supports both visual and normal mode
+
+      vim.keymap.set('n', '<leader>rc', function()
+        require('refactoring').debug.cleanup {}
+      end)
+      -- Supports only normal mode
     end,
   },
 }
