191dba5ba0c4be6dfae6fc599a58e66696d103c1
magnus <magnus.wahlstrand@gmail.com>
2025-01-07 21:49:40 +0100

    add neotree and nice landing zone, and non working tmux integration

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,28 @@
+-- Copied from Joakim https://github.com/mrlorentx/.files/blob/main/nvim/lua/custom/plugins/neo-tree.lua
+return {
+
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal right<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+      },
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
